0eb7a0e165ea874f48c7718965fcf97328af69da
e-h-dev <ebaal8226@gmail.com>
2026-05-04 12:52:44 +0100

    Update urls.py

--- rentals/urls.py
+++ rentals/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.rentals, name='rentals'),
     path('<int:rental_id>', views.rental_detail, name='rental_detail'),
     path('list_home', views.list_home, name='list_home'),
+    path('load_images/<int:rental_id>', views.load_images, name='load_images'),
 ]
