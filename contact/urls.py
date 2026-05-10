510dbb78d65407474f304eddf3302c6a534ed371
e-h-dev <ebaal8226@gmail.com>
2026-05-10 12:19:54 +0100

    Add functions and urls to contacts app

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,8 @@
-from .views import contacts
+from .views import contacts, read, delete_message
 from django.urls import path
 
 urlpatterns = [
     path('', contacts, name='contacts'),
+    path('read/<int:contact_id>', read, name='read'),
+    path('delete_message/<int:contact_id>', delete_message, name='delete_message'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Contacts
 
 # Create your views here.
@@ -7,3 +8,17 @@ def contacts(request):
     contacts = Contacts.objects.all()
     context = {'contacts': contacts}
     return render(request, 'contact/contacts.html', context)
+
+
+def read(request, contact_id):
+
+    contact = get_object_or_404(Contacts, pk=contact_id)
+    contact.read = True
+    contact.save()
+    return HttpResponse(status=204)
+
+
+def delete_message(request, contact_id):
+    contact = get_object_or_404(Contacts, pk=contact_id)
+    contact.delete()
+    return HttpResponse(status=204)
