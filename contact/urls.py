28417a35daebd29d681f42a1890901df692d6d1d
e-h-dev <ebaal8226@gmail.com>
2026-05-11 12:07:09 +0100

    Create views and urls for compose message

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,9 @@
-from .views import contacts, read, delete_message
+from .views import compose_message, contacts, read, delete_message
 from django.urls import path
 
 urlpatterns = [
     path('', contacts, name='contacts'),
+    path('compose/<int:user_id>', compose_message, name='compose_message'),
     path('read/<int:contact_id>', read, name='read'),
     path('delete_message/<int:contact_id>', delete_message, name='delete_message'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from contact.forms import ContactForm
 from .models import Contacts
 
 # Create your views here.
+
 
 def contacts(request):
     contacts = Contacts.objects.all()
     context = {'contacts': contacts}
     return render(request, 'contact/contacts.html', context)
+
+
+def compose_message(request, user_id):
+
+    # compose = Contacts.objects.filter(send_to_id=user_id)
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.send_to_id = user_id
+            contact.save()
+            return HttpResponse(status=204)
+
+    return render(request, 'contact/compose_message.html')
 
 
 def read(request, contact_id):
