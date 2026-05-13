8e825c57afcb7d604e1668b54763f42d0fa2162d
e-h-dev <ebaal8226@gmail.com>
2026-05-13 13:06:56 +0100

    Remove placeholders

--- contact/forms.py
+++ contact/forms.py
@@ -9,7 +9,4 @@ class ContactForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs.update({'placeholder': 'Your Name'})
-        self.fields['email'].widget.attrs.update({'placeholder': 'Your Email'})
-        self.fields['subject'].widget.attrs.update({'placeholder': 'Subject'})
-        self.fields['message'].widget.attrs.update({'placeholder': 'Your Message', 'rows': 35})
+        
