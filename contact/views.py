c506b9619f63539043e9d127923adbef8e27f456
e-h-dev <ebaal8226@gmail.com>
2026-05-25 11:40:51 +0100

    Update message count system

--- contact/views.py
+++ contact/views.py
@@ -10,7 +10,19 @@ from rentals.models import Rentals
 
 def contacts(request):
     contacts = Contacts.objects.all()
-    context = {'contacts': contacts}
+
+    current_user_id = request.user.id
+    my_contacts = Contacts.objects.filter(send_to=current_user_id)
+    message_count = len(my_contacts)
+
+    unread_messages = my_contacts.filter(read=False)
+    unread_message_count = len(unread_messages)
+    
+    context = {
+        'contacts': contacts,
+        'message_count': message_count,
+        'unread_message_count': unread_message_count
+        }
     return render(request, 'contact/contacts.html', context)
 
 
