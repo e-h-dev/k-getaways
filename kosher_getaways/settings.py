629328688df8ba2505c13c26ccb7f6d2c5cea0c8
E Hammond <123422531+e-h-dev@users.noreply.github.com>
2026-06-03 13:41:06 +0000

    Fix whitenoise asset compiling error

--- kosher_getaways/settings.py
+++ kosher_getaways/settings.py
@@ -31,7 +31,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.render.com', 'localhost', '127.0.0.1']
 
@@ -240,7 +240,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
