941fe9931c5cca94d699cf3858a84a0ec22802a9
E Hammond <123422531+e-h-dev@users.noreply.github.com>
2026-06-24 12:02:40 +0000

    Create unique admin list for rentals

--- rentals/admin.py
+++ rentals/admin.py
@@ -3,7 +3,16 @@ from .models import Location, Category, Rentals, Image, AvailableDates
 
 # Register your models here.
 
-admin.site.register(Rentals)
+class RentalsAdmin(admin.ModelAdmin):
+    list_display = (
+        'active',
+        'date_added',
+        'owner_name', 
+        'owner_number',
+        'owner_email',
+    )
+
+admin.site.register(Rentals, RentalsAdmin)
 admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Location)
