651143395fa91646719f85aa08ea7f3f07915e7d
E Hammond <123422531+e-h-dev@users.noreply.github.com>
2026-07-19 18:17:56 +0000

    create custom email for new contact notification

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
 from contact.forms import ContactForm
 from .models import Contacts
 from rentals.models import Rentals
@@ -36,15 +37,34 @@ def compose_message(request, rental_id):
             sender_name = form.cleaned_data.get('name')
             messages.success(request, "Your message has been sent.")
 
-            send_mail(
-                 'New Message',
-                    f"Dear {rental.owner_name}! \
-                        You have recieved a message from {sender_name} in regard to you home '{rental.title}' listed on Kosher Getaways. \
-                        Checkout you messages to read you new message",
-                    "office@koshergetaways.co.uk",
-                    [rental.owner_email],
-                    fail_silently=False,
+            message_subject = contactform.subject
+            message = contactform.message
+            message_email = contactform.email
+
+            subject = "New Message"
+            from_email = "office@koshergetaways.co.uk"
+            to = [rental.owner_email]
+
+            text_content = (
+                f"Dear {rental.owner_name},\n"
+                f"You have successfully edited your home listing '{rental.title}' on Kosher Getaways.\n"
+                f"If you have any questions, contact us at office@koshergetaways.co.uk."
             )
+
+        
+            # Render HTML template
+            html_content = render_to_string("emails/contact_email.html", {
+                "owner_name": rental.owner_name,
+                "title": rental.title,
+                "message_subject": message_subject,
+                "message": message,
+                "message_email": message_email,
+                "footer_image_url": "https://www.koshergetaways.co.uk/static/media/logo_3.png",
+            })
+
+            msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
             return redirect('home')  # Redirect to the home page after sending the message
 
     else:
